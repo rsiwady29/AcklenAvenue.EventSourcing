a694de3620d807900d5bad269441018f2c22314c
Byron Sommardahl <byron@acklenavenue.com>
2015-06-02 16:05:23 -0500

    Added in memory store.

---
+++ src/AcklenAvenue.EventSourcing/InMemoryEventStore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcklenAvenue.EventSourcing
+{
+    public class InMemoryEventStore : IEventStore
+    {
+        public static readonly List<QueueItem> Items = new List<QueueItem>();
+
+        public Task<IEnumerable<object>> GetStream(Guid aggregateId)
+        {
+            var task = new Task<IEnumerable<object>>(
+                () =>
+                {
+                    var queueItems = Items.Where(x => x.AggregateId == aggregateId);
+                    var domainEvents = queueItems.OrderBy(x => x.Time).Select(x => x.Event);
+                    return domainEvents;
+                });
+
+            task.Start();
+
+            return task;
+        }
+
+        public void Persist(Guid aggregateId, object @event)
+        {
+            Items.Add(new QueueItem(aggregateId, @event, DateTime.Now));
+        }
+    }
+}
---
+++ src/AcklenAvenue.EventSourcing/QueueItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AcklenAvenue.EventSourcing
+{
+    public class QueueItem
+    {
+        public QueueItem(Guid aggregateId, object @event, DateTime time)
+        {
+            AggregateId = aggregateId;
+            Event = @event;
+            Time = time;
+        }
+
+        public Guid AggregateId { get; set; }
+        public object Event { get; set; }
+        public DateTime Time { get; set; }
+    }
+}

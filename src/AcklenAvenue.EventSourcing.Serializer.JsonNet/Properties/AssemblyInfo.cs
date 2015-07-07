eff9b86e0040e169dac0b2d276516011cb376832
Byron Sommardahl <byron@acklenavenue.com>
2015-07-07 17:06:14 -0500

    Adding descr.

--- src/AcklenAvenue.EventSourcing.Serializer.JsonNet/Properties/AssemblyInfo.cs
+++ src/AcklenAvenue.EventSourcing.Serializer.JsonNet/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AcklenAvenue.EventSourcing.Serializer.JsonNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Json.net serializer for events.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AcklenAvenue.EventSourcing.Serializer.JsonNet")]
